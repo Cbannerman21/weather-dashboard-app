2b4b954e8cdc7e194252e94534f6bb476ab9f9f7
Cbannerman <cbannerman21@gmail.com>
2022-07-22 19:02:03 -0400

    added local storage

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-var cityInputEl = document.querySelector("#city");
+var cityInputEl = document.querySelector("#city-name");
 var searchInputEl = document.querySelector("#search");
 var currentCityEl = document.querySelector("#current-city");
 var currentTempEl = document.querySelector("#current-temp");
@@ -11,7 +11,9 @@ var cityArray = JSON.parse(localStorage.getItem("cityArray")) || [];
 
 
 
-const getCurrentWeather = async function() {
+const getCurrentWeather = async() => {
+
+    
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInputEl.value.trim()}&units=imperial&appid=beff4d2d3a48b96528fcd84d9d9be94a`;
     const res = await fetch(apiUrl);
     let data = await res.json();
@@ -23,11 +25,45 @@ const getCurrentWeather = async function() {
     currentWindEl.textContent = "Wind: " + data.wind.speed + "mph";
     currentHumidityEl.textContent = "Humidity: " + data.main.humidity + "%";
     currentWeatherImg.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-};
+
+    var inputVal = document.getElementById("city-name").value;
+    searchInputEl.textContent = inputVal;
+
+    if (cityArray.indexOf(inputVal) === 0) {
+    cityArray.push(inputVal);
+}
+
+    localStorage.setItem("cityArray", JSON.stringify(cityArray));
+
+    var loadCity = localStorage.getItem("cityArray");
+    pastCity.innerHTML = loadCity;
+    };
+
+    var getWeatherData = function (lat, lon) {
+        var apiUrl =
+          "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+          lat +
+          "&lon=" +
+          lon +
+          "&exclude=minutely,hourly&units=imperial&appid=" +
+          apiKey +
+          " ";
+        fetch(apiUrl)
+          .then((response) => response.json())
+          .then((data) => {
+            showData(data);
+            console.log(data);
+          });
+      };
+      
+     
+    
+
 
 var getWeather = async (event) => {
     event.preventDefault();
     getCurrentWeather();
+    
 };
 
 searchInputEl.addEventListener('submit', getWeather);
